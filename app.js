4c22b063edce8b1a2b7125b6b832199c17fbd042
Can <78204287+jenkem1337@users.noreply.github.com>
2021-07-20 14:04:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -45,16 +45,30 @@ class BookData{
         this.book.setComplete(false)
         
         
-        this.datas.push({
+       this.datas.push({
             book:     this.book.getName(),
             auther:   this.book.getAuther(),
             price:    this.book.getPrice(),
             complete: this.book.getComplete()   
         })
-
+        
+        
     }
     deleteBook(index){
         this.datas.splice(index,1)
+
+    }
+
+    updateBook(bookName, auther, price, index){
+        this.book.setName(bookName)
+        this.book.setAuther(auther)
+        this.book.setPrice(price)
+        this.book.setComplete(false)
+
+        this.datas[index].book = this.book.getName()
+        this.datas[index].auther = this.book.getAuther()
+        this.datas[index].price = this.book.getPrice()
+        this.datas[index].complete = this.book.getComplete()
     }
     
     toggleBook(index){
@@ -83,9 +97,14 @@ class BookController{
         this.bookData.deleteBook(index)
     }
 
+    updateBookData(bookName, auther, price, index){
+        this.bookData.updateBook(bookName, auther, price, index)
+    }
+
     returnBookData(){
         return this.bookData.datas
     }
+
     toggleBookData(index){
         this.bookData.toggleBook(index)
     }
@@ -96,28 +115,25 @@ class BookController{
 //View 
 const book = new BookController(new BookData(new Book))
 
+const addBook = (event) => {
+    event.preventDefault()
+    const isimInput = document.getElementById("kitap-input")
+    const yazarInput = document.getElementById("yazar-input")
+    const fiyatInput = document.getElementById("fiyat-input")
 
-const isimInput = document.getElementById("kitap-input")
-const yazarInput = document.getElementById("yazar-input")
-const fiyatInput = document.getElementById("fiyat-input")
-const ulTag = document.getElementById("list")
-
-
-const addBook = () => {
     if(isimInput.value !="" && yazarInput.value !="" && fiyatInput.value!=""){
 
         book.addBook(isimInput.value, yazarInput.value ,fiyatInput.value)
 
-        Render()
-
         isimInput.value = ""
         yazarInput.value = ""
         fiyatInput.value = "" 
+        Render()
     }else{
         isimInput.value = ""
         yazarInput.value = ""
         fiyatInput.value = "" 
-        return console.log(false)
+        console.error(false)
     }
 }
 
@@ -134,22 +150,54 @@ const itsFinished = (event, index) => {
     Render()
 }
 
+const UpdateBook = (event,index) => {
+    event.preventDefault()
+
+    const newBookName = document.querySelector(".newbook")
+    const newBookAuther = document.querySelector(".newauther")
+    const newBookPrice = document.querySelector(".newprice")
+
+    if(newBookName.value !="" || newBookAuther.value !="" || newBookPrice.value !=""){
+        book.updateBookData(newBookName.value, newBookAuther.value, newBookPrice.value ,index)
+        Render()
+        newBookName.value = ""
+        newBookAuther.value= ""
+        newBookPrice.value= ""
+    }
+}
+
+const openUpdateForm = (event) => {
+    event.preventDefault()
+    const updateBox = document.getElementById("update")
+    const updateInput = document.getElementById("form-btn")
+    if(updateBox.style.display == "none"){
+        updateBox.style.display = "block"
+        updateInput.innerHTML = "Close Update Form"
+    }else{
+        updateBox.style.display = "none"
+        updateInput.innerHTML = "Open Update Form"
+    }
+    Render()   
+}
 
 const Render = () => {
     let template = ""
-    book.returnBookData().map((data, index) =>{
+    const ulTag = document.getElementById("list")
+    book.returnBookData().map( (data, index) => {
         template += `
             <li id="lists">
-                Kitabın İsmi           : ${data.book} <br>
-                Kitabın Yazarı         : ${data.auther}<br>
-                Kitabın Fiyatı         : ${data.price} TL<br>
-                Kitap Okundu Mu ?      : ${data.complete == false ? "Okunmadı" : "Okundu" }
-             </li>
+                Book Name                : ${data.book} <br>
+                Book Auther              : ${data.auther}<br>
+                Book Price               : ${data.price} TL<br>
+                Is that book read ?      : ${data.complete == false ? "It is not read" : "It has been read" } <br> <br>
 
-             <div class="buttons">
-                <button class="sil-btn" onclick="Delete(event,${index})">Sil</button>
-                <button class="güncelle-btn" onclick="itsFinished(event, ${index})">Okundu Olarak İşaretle</button>
-            </div>
+
+                <div class="buttons">
+                    <button onclick="Delete(event,${index})">Delete</button>
+                    <button onclick="itsFinished(event, ${index})">Toggle</button>
+                    <button onclick="UpdateBook(event, ${index})">Update Book</button>
+                </div>                        
+            </li>        
         `
     })
     ulTag.innerHTML = template
